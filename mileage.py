457a6ad6f8079962b28e93b5e5545e6199091304
Henry-Allucard <72110643+Henry-Allucard@users.noreply.github.com>
2020-10-10 01:40:49 -0400

    Update mileage.py

--- mileage.py
+++ mileage.py
@@ -1,4 +1,5 @@
 print("How many kilometers did you run or cycle today?")
 kms=input()
 miles=float(kms)/1.60934
-print(f"okay you said:{kms}kilometers and this is equal to:{miles}miles")
+miles= round(miles,2)
+print(f"okay you said: {kms} km and this is equal to: {miles} mi")
